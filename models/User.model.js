5bbb4711ba0c02e85543ebfe96d7694ba252b127
Begum Eryildiz <begumeryildiz@gmail.com>
2022-07-18 11:00:10 +0200

    added username to user model

--- models/User.model.js
+++ models/User.model.js
@@ -2,6 +2,10 @@ const { Schema, model } = require('mongoose');
 
 const userSchema = new Schema(
   {
+    username: {
+      type: String,
+      required: [true, 'Username is required.']
+    },
     email: {
       type: String,
       required: [true, 'Email is required.'],
